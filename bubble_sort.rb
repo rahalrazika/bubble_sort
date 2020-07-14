3c0fdae534a56c16823bd62b25decdcfea11f6a7
rahalrazika <rere055@gmail.com>
2020-07-14 17:18:49 +0200

    add the ruby code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,13 @@
-def bubble_sort(arr) 
-    i= 0 
-   until i > arr.length-1   
-          if arr[i] > arr[i+1]
-          arr[i], arr[i+1] = arr[i+1], arr[i]
-          switch = true
-          i += 1
-          end 
-          
-       break if arr.length-1
-       
-      end
-    return arr
-    end  
-    p  bubble_sort([12,10,78,2,0,2])
-  
+def bubble_sort(arr)
+  i = 0
+  until i == arr.length -1
+  if arr[i] <= arr[i + 1]
+    i += 1
+   elsif arr[i] > arr[i + 1]
+    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    i = 0
+   end
+  end
+  arr
+end
+ p bubble_sort([12, 10, 78, 2, 0, 2])
