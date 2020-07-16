a30ff2fb1b86464c21a02702da7e9f3e81b6c7cb
abidoyinsola1 <ifeobayemi@gmail.com>
2020-07-16 15:26:07 +0100

    fix linters error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -24,6 +24,6 @@ def bubble_sort_by(arr)
   end
   arr
 end
-bubble_sort_by(%w[hi hello hey]) { |left, right|
+bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
-}
+end
